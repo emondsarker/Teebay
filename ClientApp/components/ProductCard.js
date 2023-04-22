5f98a7a96a72b144b58e149a64ba59a9a6d8696b
emondsarker <emon331@gmail.com>
2023-04-22 18:32:00 +0600

    /all-products page is connected to BE, utilizes ProductCard

--- ClientApp/components/ProductCard.js
+++ ClientApp/components/ProductCard.js
@@ -1,8 +1,18 @@
 import { Grid, Box, Card, Typography } from "@mui/material"
 import DeleteIcon from '@mui/icons-material/Delete';
 
-export default function ProductCard( data ){
-    console.log(data)
+export default function ProductCard(data) {
+
+
+    const cardData = {
+        title: data.data.title,
+        description: data.data.description,
+        categories: data.data.categories,
+        price: data.data.price,
+        rent: data.data.rent,
+        rentInterval: data.data.rentInterval,
+    }
+    console.log(cardData)
     // Typography themes
     const titleStyle = {
         mr: 2,
@@ -11,7 +21,7 @@ export default function ProductCard( data ){
         letterSpacing: '.1rem',
         color: 'inherit',
         textDecoration: 'none',
-      }
+    }
     const greyedOut = {
         color: 'grey',
         textDecoration: 'none',
@@ -23,35 +33,46 @@ export default function ProductCard( data ){
         fontSize: '15px'
     }
 
-    return(
-        <Card>
+    return (
+        <Card sx={{ width: '70%', marginBottom: 3, padding: 2 }}>
             <Grid container spacing={0}>
                 <Grid item sm={11}>
                     <Typography variant="h4" sx={titleStyle}>
-                        {data.data.title}
+                        {cardData.title}
                     </Typography>
                     <Typography sx={greyedOut}>
-                        {data.data.categories}
+                        {(cardData.categories == null) ? (
+                            <></>
+                        ) : (
+                            <>
+                                {
+                                    cardData.categories.map((data) => (
+                                        <>{data.id}</>
+                                    ))
+                                }
+                            </>
+                        )}
+
                     </Typography>
                     <Typography sx={greyedOut}>
-                        Price: ${data.data.price} | Rent: ${data.data.rent} {data.data.interval}
+                        Price: ${cardData.price} | Rent: ${cardData.rent} {cardData.interval}
                     </Typography>
                     <Typography sx={description}>
-                        {data.data.description}
+                        {cardData.description}
                     </Typography>
                 </Grid>
                 <Grid item sm={1}>
-                    <DeleteIcon/>
+                    <DeleteIcon />
                 </Grid>
 
                 <Grid item sm={11}>
                     <p>
-                        Date posted: {data.data.date}
+                        {/* Date posted: {data.data.date} */}
                     </p>
                 </Grid>
                 <Grid item sm={1}>
                     <p>
-                        {data.data.views} views
+                        {/* {data.data.views} views */}
                     </p>
                 </Grid>
             </Grid>
--- ClientApp/pages/all-products.js
+++ ClientApp/pages/all-products.js
@@ -3,8 +3,11 @@ import ResponsiveAppBar from '../components/navbar'
 import { InferGetServerSidePropsType } from 'next'
 import ProductCard from '../components/ProductCard'
 import { Box } from '@mui/system'
+import { useEffect, useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
 
 export default function MyProducts() {
+  const [productData, setProductData] = useState([{}])
   const test = {
     title: "Cricket kit",
     categories: "Sporting Goods, Outdoor",
@@ -15,22 +18,73 @@ export default function MyProducts() {
     date: "21st August 2020",
     views: "156"
   }
+
+  const ALL_PRODUCTS = gql`
+  query products {
+    products {
+      title
+      description
+      categories {
+        id
+      }
+      price
+      rent
+      rentInterval
+      createdAt
+      updatedAt
+      ownerId
+    }
+  }
+`;
+
+  const { loading, error, data, refetch } = useQuery(ALL_PRODUCTS, {
+    onCompleted: (data) => {
+      // console.log(data.products[0])
+      let array = []
+      for (let i = 0; i < data.products.length; i++) {
+        array[i] = data.products[i]
+      }
+      setProductData(array)
+    }
+  })
+  console.log(productData)
+  useEffect(() => {
+
+  }, [])
+
   return (
 
     <div>
-      <ResponsiveAppBar/>
-      <Box  
-      sx={{
-            marginTop: '5%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-        <ProductCard data={test}/>
-          </Box>
-     
+      <ResponsiveAppBar />
+      <Box
+        sx={{
+          marginTop: '5%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+
+        }}>
+
+        {(productData == null) ? (
+          <></>
+        ) : (
+          <>
+            {productData.map((data) => (
+              // 
+              <>
+                <ProductCard sx={{ minWidth: '500px' }} data={data} />
+                {console.log(data)}
+              </>
+
+            ))}
+          </>
+
+        )}
+
+      </Box>
+
     </div>
-   
+
   )
 }
 
